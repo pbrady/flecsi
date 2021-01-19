a5fa7f017d4e54107cde53f3697d43820eaace17
Ben Bergen <bergen@lanl.gov>
2021-01-18 23:30:37 -0500

    Updating image structure
    - Moved more setup into os images
    - Added external graphviz
    - Removed debug build of backends
    - Added Caliper

--- cmake/image.cmake
+++ cmake/image.cmake
@@ -25,9 +25,48 @@ function(add_image target tag dockerfile)
     list(APPEND args "--build-arg;${arg}")
   endforeach()
 
+  file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/targets)
+
+  message(STATUS "Adding target: ${target}")
+  file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/targets/${target})
+
+  configure_file(${CMAKE_SOURCE_DIR}/config/dockerignore
+  ${CMAKE_BINARY_DIR}/targets/${target}/.dockerignore)
+
+  if(EXISTS ${CMAKE_SOURCE_DIR}/aux/gnu-${target}.xz)
+    file(COPY ${CMAKE_SOURCE_DIR}/aux/gnu-${target}.xz
+      DESTINATION ${CMAKE_BINARY_DIR}/targets/${target}
+    )
+  endif()
+
+  file(COPY ${CMAKE_SOURCE_DIR}/config/clang_fortran.py
+    DESTINATION ${CMAKE_BINARY_DIR}/targets/${target}
+    FILE_PERMISSIONS
+      OWNER_READ
+      OWNER_WRITE
+      OWNER_EXECUTE
+      GROUP_READ
+      GROUP_EXECUTE
+      WORLD_READ
+      WORLD_EXECUTE
+  )
+
+  file(COPY ${CMAKE_SOURCE_DIR}/config/dircolors
+    DESTINATION ${CMAKE_BINARY_DIR}/targets/${target}
+  )
+
+  file(COPY ${CMAKE_SOURCE_DIR}/config/bashrc
+    DESTINATION ${CMAKE_BINARY_DIR}/targets/${target}
+  )
+
+  file(COPY ${CMAKE_SOURCE_DIR}/config/vimrc
+    DESTINATION ${CMAKE_BINARY_DIR}/targets/${target}
+  )
+
   add_custom_target(${target} ALL
     ${ENGINE_EXECUTABLE} build \${BUILD_EXTRA} ${args} -t ${tag}
-      -f ${CMAKE_SOURCE_DIR}/${dockerfile} .
+      -f ${CMAKE_SOURCE_DIR}/${dockerfile}
+      ${CMAKE_BINARY_DIR}/targets/${target}
     DEPENDS ${dockerfile} ${image_DEPENDS})
 
   add_custom_target(push-${target} ALL
